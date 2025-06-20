21443be95e3f351e6d2eec5491c90eb6d17ee8a0
giacholari <giacholari@gmail.com>
2025-06-20 15:44:27 +0100

    WIP | read file

--- app/Http/Controllers/AffiliatesInvitationController.php
+++ app/Http/Controllers/AffiliatesInvitationController.php
@@ -8,6 +8,33 @@ class AffiliatesInvitationController extends Controller
 {
     public function invite(Request $request)
     {
-        return 'foo';
+        // $request->validate([
+        //     'affiliates' => 'required|file|mimetypes:text/plain,application/json',
+        // ]);
+    
+        $affiliates = [];
+    
+        // Get the uploaded file from the request
+        $file = $request->file('affiliates');
+    
+        // Open a stream to read the file without storing
+        if ($file->isValid()) {
+            $handle = fopen($file->getRealPath(), 'r');
+    
+            if ($handle) {
+                while (($line = fgets($handle)) !== false) {
+                    $data = json_decode(trim($line), true);
+                    if (json_last_error() === JSON_ERROR_NONE) {
+                        $affiliates[] = $data;
+                    }
+                }
+                fclose($handle);
+            }
+        }
+    
+        return response()->json([
+            'count' => count($affiliates),
+            'data' => $affiliates,
+        ]);
     }
 }
