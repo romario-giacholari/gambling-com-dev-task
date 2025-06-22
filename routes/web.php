d12eb400afd527b3e4f0fc94732a724401684856
giacholari <giacholari@gmail.com>
2025-06-22 13:56:30 +0100

    remove web route

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,3 @@ Route::get('dashboard', function () {
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
-
-Route::post('affiliates/invite', [\App\Http\Controllers\AffiliatesInvitationController::class, 'invite'])->name('affiliates.invite');
